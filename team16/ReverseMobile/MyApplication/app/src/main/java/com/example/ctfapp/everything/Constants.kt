e0498448fe85fa8fb223b7d29b5f5c2aeef44913
pavelapk <pepdj@mail.ru>
2023-07-02 22:46:44 +0700

    rebuild Team16 "ReverseMobile" with new address

--- team16/ReverseMobile/MyApplication/app/src/main/java/com/example/ctfapp/everything/Constants.kt
+++ team16/ReverseMobile/MyApplication/app/src/main/java/com/example/ctfapp/everything/Constants.kt
@@ -2,7 +2,7 @@ package com.example.ctfapp.everything
 
 object Constants {
 
-    const val BASE_URL = "http://v1904504.hosted-by-vdsina.ru:5000/api/"
+    const val BASE_URL = "http://158.160.62.132:7000/api/"
 
     val SECRET_BYTE_ARRAY: ByteArray = byteArrayOf(
     72, 73, 84, 83, 123, 100, 97, 121, 95, 102, 50,
